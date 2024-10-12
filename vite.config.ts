a8a9e086ff6856b2cb18984f2591cc82099c706c
Staffan Pedersen <staffanbp@hotmail.com>
2024-10-12 18:17:37 +0200

    update dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the mode
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    base: '/',
-    plugins: [vue(), vueJsx()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
+export default defineConfig({
+  base: '/',
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Replace this with your backend API
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   }
